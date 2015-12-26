36320ea8c78a06bea0c902d4d864610cabe9a831
masaki.haga <masaki.haga@linecorp.com>
2015-12-26 14:17:51 +0900

    Initial Commit

---
+++ FunctionalAnimation/FunctionalAnimation.h
@@ -0,0 +1,19 @@
+//
+//  FunctionalAnimation.h
+//  FunctionalAnimation
+//
+//  Created by Haga Masaki on 2015/12/26.
+//  Copyright © 2015年 Haga Masaki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FunctionalAnimation.
+FOUNDATION_EXPORT double FunctionalAnimationVersionNumber;
+
+//! Project version string for FunctionalAnimation.
+FOUNDATION_EXPORT const unsigned char FunctionalAnimationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FunctionalAnimation/PublicHeader.h>
+
+
