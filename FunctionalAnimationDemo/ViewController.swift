a8ff05291b0ce73dff01a7cdeea0030f4b1f1710
hagmas <hgmsk1985@gmail.com>
2015-12-28 17:31:27 +0900

    First commit of demo

--- FunctionalAnimationDemo/ViewController.swift
+++ FunctionalAnimationDemo/ViewController.swift
@@ -11,33 +11,157 @@ import FunctionalAnimation
 
 class ViewController: UIViewController {
 
-    let animationLayer = CALayer()
-    let maskLayer = CALayer()
+    enum State {
+        case first, second, third, fourth
+        
+        var path: CGPathRef {
+            switch self {
+            case .first:
+                return UIBezierPath(ovalInRect: CGRectMake(0, 0, 200, 200)).CGPath
+            case .second:
+                return UIBezierPath(ovalInRect: CGRectMake(0, 0, 200, 200)).CGPath
+            case .third:
+                return UIBezierPath(ovalInRect: CGRectMake(0, 0, 200, 200)).CGPath
+            case .fourth:
+                return UIBezierPath(ovalInRect: CGRectMake(0, 0, 200, 200)).CGPath
+            }
+        }
+        
+        var fillColor: CGColorRef {
+            switch self {
+            case .first:
+                return UIColor.redColor().CGColor
+            case .second:
+                return UIColor.yellowColor().CGColor
+            case .third:
+                return UIColor.blueColor().CGColor
+            case .fourth:
+                return UIColor.greenColor().CGColor
+            }
+        }
+        
+        
+        var nextState: State {
+            switch self {
+            case .first:
+                return .second
+            case .second:
+                return .third
+            case .third:
+                return .fourth
+            case .fourth:
+                return .first
+            }
+        }
+    }
+    
+    
+    let maskedLayer = CALayer()
+    let maskLayer   = CAShapeLayer()
+    let shapeLayer  = CAShapeLayer()
+    
+    var state: State = .first {
+        didSet {
+            CATransaction.begin()
+            CATransaction.setCompletionBlock { () -> Void in
+                self.view.userInteractionEnabled = true
+                self.shapeLayer.opacity = 0.0
+                self.shapeLayer.path = self.state.nextState.path
+                self.shapeLayer.fillColor = self.state.nextState.fillColor
+            }
+            maskedLayer.backgroundColor = state.fillColor
+            
+            maskLayer.path = state.path
+            
+            shapeLayer.opacity = 1.0
+            maskLayer.removeAllAnimations()
+            shapeLayer.removeAllAnimations()
+            CATransaction.commit()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        view.layer.backgroundColor = UIColor.redColor().CGColor
+        view.layer.backgroundColor = UIColor.whiteColor().CGColor
         
-        animationLayer.frame = view.bounds
-        animationLayer.backgroundColor = UIColor.blueColor().CGColor
+        maskedLayer.frame = view.bounds
+        maskedLayer.backgroundColor = state.fillColor
     
-        view.layer.addSublayer(animationLayer)
+        view.layer.addSublayer(maskedLayer)
         
-        // add mask layer
+        // Add Mask layer
         maskLayer.frame = CGRectMake(view.bounds.size.width/2 - 100, view.bounds.size.height/2 - 100, 200, 200)
-        maskLayer.backgroundColor = UIColor.blackColor().CGColor
+        maskLayer.path = state.path
+        maskLayer.fillColor = UIColor.blackColor().CGColor
         
-        animationLayer.mask = maskLayer
+        maskedLayer.mask = maskLayer
         
-        let animation = FunctionalAnimation<TransformScale>()
-        animation.fromValue = 1.0
-        animation.toValue = 2.0
-        animation.interpolatingFunction = .ExponentialGrowth(6.0)
+        // Add Shape Layer
+        shapeLayer.frame = CGRectMake(view.bounds.size.width/2 - 100, view.bounds.size.height/2 - 100, 200, 200)
+        shapeLayer.path = state.nextState.path
+        shapeLayer.fillColor = state.nextState.fillColor
+        shapeLayer.opacity = 0.0
+        shapeLayer.transform = CATransform3DMakeScale(1.5, 1.5, 1.0)
+        shapeLayer.actions = ["opacity": NSNull()]
         
+        maskedLayer.addSublayer(shapeLayer)
     }
 
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        view.userInteractionEnabled = false
+        
+        let animation = FunctionalAnimation<TransformScale>()
+        animation.duration = 0.2
+        animation.fromValue = 1.0
+        animation.toValue = 0.5
+        animation.interpolatingFunction = .ExponentialDecay(6.0)
+        animation.fillMode = kCAFillModeForwards
+        animation.removedOnCompletion = false
+        maskLayer.addAnimation(animation, forKey: nil)
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let animation = FunctionalAnimation<TransformScale>()
+        animation.duration = 1.0
+        animation.fromValue = 0.5
+        animation.toValue = 4.5
+        animation.interpolatingFunction = .ExponentialGrowth(12.0)
+        animation.fillMode = kCAFillModeForwards
+        animation.removedOnCompletion = false
+        
+        CATransaction.begin()
+        CATransaction.setCompletionBlock { () -> Void in
+            self.view.layer.backgroundColor = self.state.fillColor
+        }
+        maskLayer.addAnimation(animation, forKey: "animation")
+        CATransaction.commit()
+        
+        let opAnim = CABasicAnimation(keyPath: "opacity")
+        opAnim.fromValue = 0.0
+        opAnim.toValue = 1.0
+        
+        let scAnim = FunctionalAnimation<TransformScale>()
+        scAnim.fromValue = 0.5
+        scAnim.toValue = 1.0
+        scAnim.interpolatingFunction = .ExponentialDecay(6.0)
+        
+        let animGp = CAAnimationGroup()
+        animGp.animations = [opAnim, scAnim]
+        animGp.duration = 0.5
+        animGp.beginTime = CACurrentMediaTime() + 0.8
+        animGp.removedOnCompletion = false
+        animGp.fillMode = kCAFillModeForwards
+        
+        CATransaction.begin()
+        CATransaction.setCompletionBlock { () -> Void in
+            self.state = self.state.nextState
+        }
+        shapeLayer.addAnimation(animGp, forKey: "animGp")
+        CATransaction.commit()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
